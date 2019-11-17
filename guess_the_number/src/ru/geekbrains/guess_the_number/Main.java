fef8d8a672d4af8204618c88a4acc65e743a47c7
Olga Atlasova <oiatlasova@gmail.com>
2019-11-17 20:14:38 +0300

    Added method

--- guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
+++ guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ваша задача угадать число.");
         System.out.println("Введите число.");
-        int range = scanner.nextInt();
+        int range = 30;
         int number = (int) (Math.random() * range);
+        playLevel(range, number);
+        scanner.close();
+    }
+
+    private static void playLevel(int range, int number) {
         while (true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
@@ -22,6 +29,5 @@ public class Main {
                 System.out.println("Загаданное число больше");
             }
         }
-        scanner.close();
     }
 }
