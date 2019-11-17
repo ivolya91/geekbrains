f411fc8cd82630d4504870cf28a0a582c0a1a606
Olga Atlasova <oiatlasova@gmail.com>
2019-11-17 20:53:38 +0300

    Added a for loop

--- guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
+++ guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
@@ -7,27 +7,25 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         System.out.println("Ваша задача угадать число.");
-        System.out.println("Введите число.");
-        int range = 30;
-        int number = (int) (Math.random() * range);
-        playLevel(range, number);
-        scanner.close();
-    }
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!!!");
+            scanner.close();
+        }
 
-    private static void playLevel(int range, int number) {
-        while (true) {
-            System.out.println("Угадайте число от 0 до " + range);
-            int input_number = scanner.nextInt();
-            if (input_number == number) {
-                System.out.println("Вы угадали.");
-                break;
-            } else if (input_number > number) {
-                System.out.println("Загаданное число меньше");
-            } else {
-                System.out.println("Загаданное число больше");
+        private static void playLevel ( int range){
+            int number = (int) (Math.random() * range);
+            while (true) {
+                System.out.println("Угадайте число от 0 до " + range);
+                int input_number = scanner.nextInt();
+                if (input_number == number) {
+                    System.out.println("Вы угадали.");
+                    break;
+                } else if (input_number > number) {
+                    System.out.println("Загаданное число меньше");
+                } else {
+                    System.out.println("Загаданное число больше");
+                }
             }
         }
     }
-}
